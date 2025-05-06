849bc56c0f6cb2d4065f2d04532693faa2c6f16e
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-05-06 18:29:33 +0700

    Membuat Prosedur Input dan Fungsi luasPp bersifat publik

--- contoh.cpp
+++ contoh.cpp
@@ -6,4 +6,18 @@ class PersegiPanjang
 public: // akses modifier
     int panjang, lebar;
 
+public:
+    void input ()
+    {
+        cout << "Masukkkan panjang : ";
+        cin >> panjang;
+        cout << "Masukkkan lebar : ";
+        cin >> lebar;
+    }
+
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
+    //Lingkaran ol;
 };
