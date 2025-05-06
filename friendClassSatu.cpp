412de13de56b7aca90e98f34ad49d9d500c94570
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-05-06 19:38:55 +0700

    Membuat prosedur siswa dan orang

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -22,3 +22,18 @@ public:
     void setID(int pId);
     void displayAll(orang &a);
 };
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+
+void siswa::setID(int pId)
+{
+    id = pId;
+}
