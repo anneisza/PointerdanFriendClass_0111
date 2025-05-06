e79abad10eec6ceeba112fea359454b6b82824c8
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-05-06 18:55:00 +0700

    Menambahkan program utama

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -9,3 +9,17 @@ class mahasiswa{
     }
 
 };
+
+int main(){
+    mahasiswa mhs{1}; //Object mhs
+    mhs.showNIM(); //Member Access Operator
+
+    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
+    refMhs.nim = 2; //Member Access Operator
+    mhs.showNIM();
+
+    mahasiswa *pMhs = &mhs; //pointer Dereference pMhs
+    pMhs->nim = 3;
+    pMhs->showNIM();
+    return 0;
+}
